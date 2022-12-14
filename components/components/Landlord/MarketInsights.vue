0ae8d9ad9a82654af8793e6a0f2336ce8bcec378
AES-PF1MVXS4\AES-MinhHuy <minhhuy.hoang@asiaesolution.com>
2022-12-14 14:30:47 +0700

    Done UI market-insights

--- components/components/Landlord/MarketInsights.vue
+++ components/components/Landlord/MarketInsights.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="page page--market-insights">
         <div class="container">
-            <div v-for="article in articles" :key="article.id" class="page__container">
-                <ArticleGrid :article="article" />
+            <div class="page__container">
+                <ArticleGrid
+                    class="page-item"
+                    v-for="article in articles"
+                    :key="article.id"
+                    :article="article"
+                />
             </div>
         </div>
     </div>
@@ -20,40 +25,40 @@ export default {
             articles: [
                 {
                     id: 1,
-                    urlImage: "/img/test-01.jpg",
+                    urlImage: "https://picsum.photos/600/400.jpg?random=",
                     title: "Closers Group Real Estate",
                     content:
                         "Nestled in the prestigious District 11 at Novena, Soleil @ Sinaran is an award"
                 },
                 {
                     id: 2,
-                    urlImage: "/img/test-02.jpg",
+                    urlImage: "https://picsum.photos/600/400.jpg?random=",
                     title: "Stellar Property Advisors",
                     content: "Nassim Jade is a 10-storey boutique styled freehold development."
                 },
                 {
                     id: 3,
-                    urlImage: "/img/test-03.jpg",
+                    urlImage: "https://picsum.photos/600/400.jpg?random=",
                     title: "Keller Williams Exclusive Properties",
                     content:
                         "Residents at Watermark @ Robertson Quay can enjoy a leisure swim in the pool"
                 },
                 {
                     id: 4,
-                    urlImage: "/img/test-01.jpg",
+                    urlImage: "https://picsum.photos/700/400.jpg?random=",
                     title: "Closers Group Real Estate",
                     content:
                         "Nestled in the prestigious District 11 at Novena, Soleil @ Sinaran is an award"
                 },
                 {
                     id: 5,
-                    urlImage: "/img/test-02.jpg",
+                    urlImage: "https://picsum.photos/500/300.jpg?random=",
                     title: "Stellar Property Advisors",
                     content: "Nassim Jade is a 10-storey boutique styled freehold development."
                 },
                 {
                     id: 6,
-                    urlImage: "/img/test-03.jpg",
+                    urlImage: "https://picsum.photos/600/400.jpg?random=",
                     title: "Keller Williams Exclusive Properties",
                     content:
                         "Residents at Watermark @ Robertson Quay can enjoy a leisure swim in the pool"
@@ -77,6 +82,26 @@ export default {
                     title: "Keller Williams Exclusive Properties",
                     content:
                         "Residents at Watermark @ Robertson Quay can enjoy a leisure swim in the pool"
+                },
+                {
+                    id: 10,
+                    urlImage: "/img/test-01.jpg",
+                    title: "Closers Group Real Estate",
+                    content:
+                        "Nestled in the prestigious District 11 at Novena, Soleil @ Sinaran is an award"
+                },
+                {
+                    id: 11,
+                    urlImage: "/img/test-02.jpg",
+                    title: "Stellar Property Advisors",
+                    content: "Nassim Jade is a 10-storey boutique styled freehold development."
+                },
+                {
+                    id: 12,
+                    urlImage: "/img/test-03.jpg",
+                    title: "Keller Williams Exclusive Properties",
+                    content:
+                        "Residents at Watermark @ Robertson Quay can enjoy a leisure swim in the pool"
                 }
             ]
         }
@@ -85,9 +110,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.page--market-insights{
-    .page__container{
-        display: inline-block
+.page--market-insights {
+    .page__container {
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        width: 100%;
+        grid-gap: 2.4rem;
+    }
+}
+@media screen and (max-width: 1023px) {
+    .page--market-insights {
+        .page__container {
+            grid-template-columns: repeat(2, minmax(0, 1fr));
+
+        }
+    }
+}
+@media screen and (max-width: 767px) {
+    .page--market-insights {
+        .page__container {
+
+            grid-template-columns: repeat(1, minmax(0, 1fr));
+  
+        }
     }
 }
 </style>
--- components/components/Landlord/components/Article/ArticleGrid.vue
+++ components/components/Landlord/components/Article/ArticleGrid.vue
@@ -4,9 +4,13 @@
             <h3 class="card__title">{{ article.title }}</h3>
         </div>
         <div class="card__body">
-            <img :src="article.urlImage" alt="" class="card__image" />
-            <p class="card__content">{{article.content}}</p>
-            <v-btn class="btn btn--ghost btn--green card__button">Read More</v-btn>
+            <div class="card__image">
+                <img :src="article.urlImage" alt="" />
+            </div>
+            <div class="card__content">
+                <p>{{ article.content }}</p>
+                <a class="card__button">Read More</a>
+            </div>
         </div>
     </div>
 </template>
@@ -31,7 +35,6 @@ export default {
     box-sizing: border-box;
     background-color: var(--color-white);
     box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.04), 0px 4px 8px rgba(0, 0, 0, 0.06);
-    width: 37.4rem;
     border-radius: 2rem;
     display: flex;
     justify-content: center;
@@ -39,33 +42,67 @@ export default {
     flex-direction: column;
     .card__header {
         padding: 1.2rem 1.6rem;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        min-height: 8rem;
         .card__title {
             font-weight: 800;
             font-size: 2rem;
             line-height: 2.8rem;
             margin-bottom: 0;
+            text-align: center;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            line-clamp: 2;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
         }
     }
     .card__body {
+        width: 100%;
         .card__image {
-            width: 37.4rem;
-            height: 20rem;
-            object-fit: cover;
+            width: 100%;
+            position: relative;
+            padding-bottom: 53.4%;
+
+            img {
+                position: absolute;
+                z-index: 10;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                max-width: 100%;
+                object-fit: cover;
+
+            }
         }
         .card__content {
             padding: 1.4rem 1.6rem 2.2rem;
-            font-weight: 400;
-            font-size: 1.8rem;
-            line-height: 2.4rem;
-            margin-bottom: 0;
-            color: var(--color-black)
+            p {
+                font-weight: 400;
+                font-size: 1.8rem;
+                line-height: 2.4rem;
+                margin-bottom: 0;
+                color: var(--color-black);
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: 2;
+                line-clamp: 2;
+                -webkit-box-orient: vertical;
+                overflow: hidden;
+            }
+            .card__button {
+                text-decoration: underline;
+                margin-top: 2.2rem;
+                font-weight: 700;
+                font-size: 1.6rem;
+                color: var(--color-primary);
+                display: inline-block;
+            }
         }
-        .card__button{
-            text-decoration: underline;
-            margin: 0 1.6rem 2.5rem;
-        }
-    }
-    .card__footer {
     }
 }
 </style>
