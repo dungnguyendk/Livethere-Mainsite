352214caafccf24db02cdd9e3b3609c353e8dfcb
minhnghiadev <minhnghiadev@gmail.com>
2022-12-14 15:14:31 +0700

    Add project details for Asset inventory

---
+++ components/components/Landlord/Property/Table/TableRecord.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+    <tr>
+    <!-- Enter code here       -->
+    </tr>
+</template>
+
+<script>
+export default {
+    name: "TableRecord"
+}
+</script>
---
+++ components/components/Landlord/Property/Table/TenancyAgreementTable.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <table class="table--responsive table--tenancy-agreement">
+        <thead>
+            <tr>
+                <th>
+                    Tenancy Agreement Code
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "TenantAgreementTable"
+}
+</script>
