02c1c666f9ceb7fa8a800ed04e0a14816c0ed94f
AES-PF1MVXS4\AES-MinhHuy <minhhuy.hoang@asiaesolution.com>
2022-12-23 17:31:35 +0700

    done validation for tenancy form

--- components/components/Landlord/Inventory/Form/CreateTenancyAgreementForm.vue
+++ components/components/Landlord/Inventory/Form/CreateTenancyAgreementForm.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-    <form class="form form--create-tenancy-agreement">
+    <form class="form form--create-tenancy-agreement" @submit.prevent="submitForm">
         <div class="form__fields">
             <v-row>
                 <v-col cols="12" sm="12" md="6">
@@ -36,25 +36,79 @@
                 <v-col cols="12" sm="12" md="6">
                     <div class="form__field">
                         <label>Start Date </label>
-                        <v-text-field v-model="startDate" dense outlined hide-details />
+                        <v-menu
+                            ref="startDateMenu"
+                            v-model="startDateMenu"
+                            :close-on-content-click="false"
+                            transition="scale-transition"
+                            offset-y
+                            min-width="auto"
+                        >
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-text-field
+                                    v-model="startDate"
+                                    :error-messages="startDateErrors"
+                                    prepend-inner-icon="mdi-calendar"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    outlined
+                                    dense
+                                    hide-details
+                                />
+                            </template>
+                            <v-date-picker
+                                v-model="startDateRaw"
+                                no-title
+                                @input="startDateMenu = false"
+                            />
+                        </v-menu>
                     </div>
                 </v-col>
                 <v-col cols="12" sm="12" md="12">
                     <div class="form__field">
                         <label>End Date </label>
-                        <v-text-field v-model="endDate" dense outlined hide-details />
+                        <v-menu
+                            ref="endDateMenu"
+                            v-model="endDateMenu"
+                            :close-on-content-click="false"
+                            transition="scale-transition"
+                            offset-y
+                            min-width="auto"
+                        >
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-text-field
+                                    v-model="endDate"
+                                    :error-messages="endDateErrors"
+                                    prepend-inner-icon="mdi-calendar"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    outlined
+                                    dense
+                                    hide-details
+                                />
+                            </template>
+                            <v-date-picker
+                                v-model="endDateRaw"
+                                no-title
+                                @input="endDateMenu = false"
+                            />
+                        </v-menu>
                     </div>
                 </v-col>
                 <v-col cols="12" sm="12" md="6">
                     <div class="form__field">
                         <label>Monthly Rental </label>
-                        <v-text-field v-model="monthlyRental" dense outlined hide-details />
+                        <v-text-field v-model.trim="monthlyRental" dense outlined hide-details 
+                            :error-messages="monthlyRentalErrors"
+                        />
                     </div>
                 </v-col>
                 <v-col cols="12" sm="12" md="6">
                     <div class="form__field">
                         <label>Secure Deposit </label>
-                        <v-text-field v-model="secureDeposit" dense outlined hide-details />
+                        <v-text-field v-model.trim="secureDeposit" dense outlined hide-details 
+                        :error-messages="secureDepositErrors"
+                        />
                     </div>
                 </v-col>
                 <v-col cols="12" sm="12" md="12">
@@ -67,7 +121,7 @@
         </div>
         <div class="form__actions">
             <v-btn class="btn btn--ghost btn--gray btn--sm" @click="onClose"> Cancel</v-btn>
-            <v-btn class="btn btn--primary btn--green btn--sm"> Create</v-btn>
+            <v-btn class="btn btn--primary btn--green btn--sm" type="submit"> Create</v-btn>
         </div>
     </form>
 </template>
@@ -76,6 +130,7 @@
 import { validationMixin } from "vuelidate"
 import { required } from "vuelidate/lib/validators"
 import { setFormControlErrors } from "~/ultilities/form-validations"
+import { convertCommasToNumber, convertNumberToCommas } from "~/ultilities/helpers"
 
 export default {
     name: "CreateTenancyAgreementForm",
@@ -83,12 +138,38 @@ export default {
     validations: {
         agreementDate: {
             required
+        }, 
+        startDate: {
+            required
+        }, 
+        endDate: {
+            required
+        }, 
+        monthlyRental: {
+            required
+        }, 
+        secureDeposit: {
+            required
         }
+        
     },
     computed: {
         agreementDateErrors() {
             return setFormControlErrors(this.$v.agreementDate, "This field is required")
+        }, 
+        startDateErrors(){
+            return setFormControlErrors(this.$v.startDate, "This field is required")
+        }, 
+        endDateErrors(){
+            return setFormControlErrors(this.$v.endDate, "This field is required")
+        }, 
+        monthlyRentalErrors(){
+            return setFormControlErrors(this.$v.monthlyRental, "This field is required")
+        }, 
+        secureDepositErrors(){
+            return setFormControlErrors(this.$v.secureDeposit, "This field is required")
         }
+        
     },
     data() {
         return {
@@ -96,7 +177,11 @@ export default {
             agreementDateMenu: "",
             agreementDateRaw: "",
             startDate: "",
+            startDateMenu: "", 
+            startDateRaw: "", 
             endDate: "",
+            endDateMenu: "", 
+            endDateRaw: "", 
             monthlyRental: "",
             secureDeposit: "",
             remark: ""
@@ -105,6 +190,26 @@ export default {
     watch: {
         agreementDateRaw() {
             this.agreementDate = this.formatDate(this.agreementDateRaw)
+        }, 
+        startDateRaw(){
+            this.startDate = this.formatDate(this.startDateRaw)
+        }, 
+        endDateRaw(){
+            this.endDate = this.formatDate(this.endDateRaw)
+        }, 
+        monthlyRental(val){
+            if(!isNaN(val)){
+                this.monthlyRental = convertNumberToCommas(val)
+            }else{
+                this.monthlyRental = convertNumberToCommas(convertCommasToNumber(val))
+            }
+        }, 
+        secureDeposit(val){
+            if(!isNaN(val)){
+                this.secureDeposit = convertNumberToCommas(val)
+            }else{ 
+                this.secureDeposit = convertNumberToCommas(convertCommasToNumber(val))
+            }
         }
     },
 
@@ -115,10 +220,9 @@ export default {
         },
         onClose() {
             this.$emit("close")
-        }
-    },
-
-    onSubmit() {
+        },
+        submitForm() {
+        console.log("submit!", this.$v.$invalid)
         this.$v.$touch()
         if (this.$v.$invalid) {
             console.log({ Error: "Form validation failed" })
@@ -126,6 +230,8 @@ export default {
             // continue actions
         }
     }
+    },
+    
 }
 </script>
 <style lang="scss" scoped>
