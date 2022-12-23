a1ee7e8a0f0168a41813d61f1170f8dd975e40fd
minhnghiadev <minhnghiadev@gmail.com>
2022-12-23 16:29:33 +0700

    Updated CreateTenancyAgreementForm validation

--- components/components/Landlord/Inventory/Form/CreateTenancyAgreementForm.vue
+++ components/components/Landlord/Inventory/Form/CreateTenancyAgreementForm.vue
@@ -4,38 +4,63 @@
             <v-row>
                 <v-col cols="12" sm="12" md="6">
                     <div class="form__field">
-                        <label>Agreement Date </label>
-                        <v-text-field v-model="agreementDate" dense outlined />
+                        <label class="required">Agreement Date </label>
+                        <v-menu
+                            ref="agreementDateMenu"
+                            v-model="agreementDateMenu"
+                            :close-on-content-click="false"
+                            transition="scale-transition"
+                            offset-y
+                            min-width="auto"
+                        >
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-text-field
+                                    v-model="agreementDate"
+                                    :error-messages="agreementDateErrors"
+                                    prepend-inner-icon="mdi-calendar"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    outlined
+                                    dense
+                                    hide-details
+                                />
+                            </template>
+                            <v-date-picker
+                                v-model="agreementDateRaw"
+                                no-title
+                                @input="agreementDateMenu = false"
+                            />
+                        </v-menu>
                     </div>
                 </v-col>
                 <v-col cols="12" sm="12" md="6">
                     <div class="form__field">
                         <label>Start Date </label>
-                        <v-text-field v-model="startDate" dense outlined />
+                        <v-text-field v-model="startDate" dense outlined hide-details />
                     </div>
                 </v-col>
-                <v-col cols="12" sm="12" md="6">
+                <v-col cols="12" sm="12" md="12">
                     <div class="form__field">
                         <label>End Date </label>
-                        <v-text-field v-model="endDate" dense outlined />
+                        <v-text-field v-model="endDate" dense outlined hide-details />
                     </div>
                 </v-col>
                 <v-col cols="12" sm="12" md="6">
                     <div class="form__field">
                         <label>Monthly Rental </label>
-                        <v-text-field v-model="monthlyRental" dense outlined />
+                        <v-text-field v-model="monthlyRental" dense outlined hide-details />
                     </div>
                 </v-col>
-                <v-col cols="12" sm="12" md="12">
+                <v-col cols="12" sm="12" md="6">
                     <div class="form__field">
                         <label>Secure Deposit </label>
-                        <v-text-field v-model="secureDeposit" dense outlined />
+                        <v-text-field v-model="secureDeposit" dense outlined hide-details />
                     </div>
                 </v-col>
                 <v-col cols="12" sm="12" md="12">
                     <div class="form__field">
                         <label>Remark </label>
-                        <v-textarea v-model="remark" dense outlined />
+                        <v-textarea v-model="remark" dense outlined hide-details />
                     </div>
                 </v-col>
             </v-row>
@@ -48,12 +73,28 @@
 </template>
 
 <script>
+import { validationMixin } from "vuelidate"
+import { required } from "vuelidate/lib/validators"
+import { setFormControlErrors } from "~/ultilities/form-validations"
+
 export default {
     name: "CreateTenancyAgreementForm",
-
+    mixins: [validationMixin],
+    validations: {
+        agreementDate: {
+            required
+        }
+    },
+    computed: {
+        agreementDateErrors() {
+            return setFormControlErrors(this.$v.agreementDate, "This field is required")
+        }
+    },
     data() {
         return {
             agreementDate: "",
+            agreementDateMenu: "",
+            agreementDateRaw: "",
             startDate: "",
             endDate: "",
             monthlyRental: "",
@@ -61,9 +102,28 @@ export default {
             remark: ""
         }
     },
+    watch: {
+        agreementDateRaw() {
+            this.agreementDate = this.formatDate(this.agreementDateRaw)
+        }
+    },
+
     methods: {
+        formatDate(date) {
+            if (!date) return null
+            return this.$moment(date).format("DD-MMM-YYYY")
+        },
         onClose() {
             this.$emit("close")
+        }
+    },
+
+    onSubmit() {
+        this.$v.$touch()
+        if (this.$v.$invalid) {
+            console.log({ Error: "Form validation failed" })
+        } else {
+            // continue actions
         }
     }
 }
@@ -76,6 +136,7 @@ export default {
         align-items: center;
         grid-gap: 1.2rem;
         gap: 1.2rem;
+        padding-top: 2.4rem;
 
         .btn {
             min-width: 12rem;
