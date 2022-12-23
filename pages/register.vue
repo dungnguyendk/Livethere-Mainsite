5fd88f5ed10dfaba010b2292f9576fee86e0354f
minhnghiadev <minhnghiadev@gmail.com>
2022-12-23 16:15:48 +0700

    Refactor register page

---
+++ pages/register.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <LandlordHeader />
+        <LandlordRegisterBenefits />
+    </div>
+</template>
+
+<script>
+import LandlordSignIn from "~/components/components/Landlord/Auth/SignIn/LandlordSignIn.vue"
+import LandlordHeader from "~/components/shared/Header/LandlordHeader.vue"
+import LandlordRegisterBenefits from "~/components/components/Landlord/Auth/Register/LandlordRegisterBenefits.vue"
+
+export default {
+    components: {
+        LandlordRegisterBenefits,
+        LandlordHeader,
+        LandlordSignIn
+    }
+}
+</script>
