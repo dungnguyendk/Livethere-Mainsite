48f7a5a1e206c2e19335520f0a0f2feb8984bed2
HAI VU <thienhai.vu@asiaesolutions.com>
2023-01-06 11:10:23 +0700

    chart inventory update pending

--- components/components/Landlord/Dashboard/components/Chart/InventoryChart.vue
+++ components/components/Landlord/Dashboard/components/Chart/InventoryChart.vue
@@ -20,6 +20,8 @@
                 </div>
                 <label class="name-progress">Total</label>
             </div>
+            <!-- <Doughnut :chart-options="chartOptionsDoughnut" :chart-data="chartDataDoughnut"
+                :plugins="pluginsDoughnut" /> -->
             <div class="section__column">
                 <div class="progress progress--blue" :data-percentage="tenant">
                     <span class="progress-left">
@@ -59,10 +61,85 @@
 </template>
 
 <script>
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale, BarElement } from "chart.js";
 import { mapState } from "vuex"
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale, BarElement);
 
 export default {
     name: "InventoryChart",
+    props: {
+        chartId: {
+            type: String,
+            default: 'doughnut-chart-total'
+        },
+        datasetIdKey: {
+            type: String,
+            default: 'label-total'
+        },
+        width: {
+            type: Number,
+            default: 400
+        },
+        height: {
+            type: Number,
+            default: 400
+        },
+        cssClasses: {
+            default: '',
+            type: String
+        },
+        styles: {
+            type: Object,
+            default: () => { }
+        },
+        plugins: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data() {
+        return {
+            chartOptionsDoughnut: {
+                maintainAspectRatio: false,
+                aspectRatio: 1,
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        enabled: false
+                    },
+                },
+            },
+            pluginsDoughnut: [{
+                // beforeDraw: function (chart, option) {
+                //     const { ctx, data, chartArea: { top, bottom, left, right, width, height }, scales: { r } } = chart
+                //     ctx.save()
+                //     const xCoor = chart.getDatasetMeta(0).data[0].x;
+                //     const yCoor = chart.getDatasetMeta(0).data[0].y;
+                //     const score = data.datasets[0].data[0]
+                //     // ctx.fillRect(xCoor, yCoor, 400, 1)
+                //     ctx.font = '20px sans-serif'
+                //     ctx.textBaseLine = "middle"
+                //     ctx.textAlign = "center"
+                //     ctx.fillText(score, xCoor, yCoor)
+                // }
+                afterDatasetsDraw(chart, args, pluginOptions) {
+                    const { ctx } = chart;
+                    ctx.textAlign = "center"
+                    ctx.textBaseLine = "middle"
+                    ctx.font = '20px sans-serif'
+
+                    const text = "I'm In the Center"
+                    const x = chart.getDatasetMeta(0).data[0].x;
+                    const y = chart.getDatasetMeta(0).data[0].y;
+                    ctx.fillText(text, x, y)
+                    ctx.fillRect(x, y, 10, 10)
+                }
+            }]
+        }
+    },
     computed: {
         ...mapState({
             dashboard: (state) => state.dashboard.dashBoards
@@ -87,7 +164,37 @@ export default {
                     100
                 ).toFixed()
                 : 0
-        }
+        },
+        // pluginsDoughnut: [{
+        //     afterDatasetsDraw(chart, args, pluginOptions) {
+        //         const { ctx } = chart;
+        //         ctx.font = '20px sans-serif'
+        //         const text = "I'm In the Center"
+        //         const x = chart.getDatasetMeta(0).data[0].x;
+        //         const y = chart.getDatasetMeta(0).data[0].y;
+        //         ctx.fillText(text, x, y)
+        //         ctx.fillRect(x, y, 10, 10)
+        //     }
+        // }],
+        chartDataDoughnut() {
+            let chartDataDoughnut = {
+                labels: [],
+                datasets: [{
+                    label: [],
+                    data: [this.dashboard.totalInventories],
+                    backgroundColor: ["#27A857"],
+                    hoverBackgroundColor: ["#27A857"],
+                    borderWidth: 0,
+                    cutout: "80%",
+                }],
+            }
+            // chartDataDoughnut.datasets[0].data = []
+            return chartDataDoughnut
+        },
+    },
+    mounted() {
+    },
+    methods: {
     }
 }
 </script>
