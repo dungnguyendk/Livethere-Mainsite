5e537e92c3ed2da26eef22510b5109c2e51953a0
minhnghiadev <minhnghiadev@gmail.com>
2023-02-22 17:46:58 +0700

    Update drawer

--- components/shared/Header/LandlordMobileHeader.vue
+++ components/shared/Header/LandlordMobileHeader.vue
@@ -9,45 +9,12 @@
             <SiteLogo />
         </div>
         <div class="header__right">
-            <v-btn icon class="btn--search"  elevation="0" @click.stop="drawerUser = !drawerUser">
-                <i class="ri-user-line"></i>
+            <v-btn icon class="btn--search" elevation="0" @click.prevent="userDrawer = !userDrawer">
+                <i class="ri-user-line" />
             </v-btn>
         </div>
         <MobileNavigation />
-        <v-navigation-drawer class="drawer ps-drawer" v-model="drawerUser" absolute temporary>
-        <div>
-            <v-btn class="ps-drawer__close"  @click.stop="drawerUser = !drawerUser" variant="text">
-                <i class="icon-svg svg-close"></i>
-            </v-btn>
-        </div>
-        <div class="ps-drawer__content">
-            <template v-if="loggedIn">
-                <v-list v-if="userInfo" class="ps-drawer__user">
-                    <v-list-item>
-                        <nuxt-link to="/landlord"> Dashboard</nuxt-link>
-                    </v-list-item>
-                    <v-list-item>
-                        <a href="/landlord/change-password" @click.prevent="onChangePassword">
-                            Change password
-                        </a>
-                    </v-list-item>
-                    <v-list-item>
-                        <a href="/" @click.prevent="onLogout">Logout</a>
-                    </v-list-item>
-                </v-list>
-            </template>
-            <template v-else>
-                <v-list class="ps-drawer__user">
-                    <v-list-item>
-                        <nuxt-link to="/landlord/signin" class="header__link"> Login </nuxt-link>
-                    </v-list-item>
-                    <v-list-item>
-                        <nuxt-link to="/register" class="header__link"> Register</nuxt-link>
-                    </v-list-item>
-                </v-list>
-            </template>
-        </div>
-    </v-navigation-drawer>
+        <LandlordUserDrawer :open="userDrawer" @close="userDrawer = false" />
     </header>
 </template>
 
@@ -56,10 +23,11 @@ import { mapState } from "vuex"
 import SiteLogo from "~/components/shared/Logo/SiteLogo.vue"
 import MobileNavigation from "~/components/shared/Drawer/MobileNavigation.vue"
 import LandlordSignIn from "~/components/components/Landlord/Auth/SignIn/LandlordSignIn.vue"
+import LandlordUserDrawer from "~/components/shared/Drawer/LandlordUserDrawer.vue"
 
 export default {
     name: "LandlordMobileHeader",
-    components: { LandlordSignIn, SiteLogo, MobileNavigation },
+    components: { LandlordUserDrawer, LandlordSignIn, SiteLogo, MobileNavigation },
     computed: {
         ...mapState({
             appDrawer: (state) => state.app.appDrawer,
@@ -71,11 +39,14 @@ export default {
     },
     data() {
         return {
-            drawerUser: false
+            userDrawer: false
         }
     },
 
     methods: {
+        onChangePassword() {
+            this.$router.push("/landlord/change-password")
+        },
         onOpenExternalSearch() {
             window.open("https://www.livethere.com/search/results?from=home")
         },
@@ -86,7 +57,7 @@ export default {
             await this.$auth.logout().then(() => {
                 window.location.href = "/landlord/signin"
             })
-        },
+        }
     }
 }
 </script>
@@ -163,39 +134,49 @@ export default {
         display: none;
     }
 }
+
 .dialog-search {
     border-radius: 0;
     margin: 0;
+
     .v-btn--icon {
         border-radius: 0.4px;
         position: absolute;
         top: 1rem;
         right: 1rem;
+
         .icon-svg {
             background-color: #828586;
         }
     }
+
     .v-card__text {
         padding-top: 6.5rem;
     }
+
     :deep(.v-input) {
         fieldset {
             // color: #DFE0E0;
         }
+
         .svg-map {
             background-color: #e7b242;
         }
+
         .v-label {
             font-size: 1.6rem;
             color: #001327;
         }
+
         ::placeholder {
             font-size: 1.6rem;
             color: #001327;
         }
+
         .v-input__slot {
             min-height: 4.8rem !important;
         }
+
         .v-input__prepend-inner {
             margin-top: 1.2rem;
             margin-right: 0.4rem;
@@ -206,9 +187,11 @@ export default {
 .v-dialog--fullscreen {
     margin: 0;
 }
+
 .ps-drawer {
     width: 100% !important;
     background-color: #0b0c0c;
+
     .ps-drawer__close {
         position: absolute;
         top: 1rem;
@@ -216,16 +199,18 @@ export default {
         background-color: transparent;
         padding: 0;
         min-width: 4.4rem;
+
         i {
             font-size: 3.8rem;
             color: white;
         }
     }
+
     .ps-drawer__content {
-        display: block;
         display: flex;
         flex-direction: column;
         padding-top: 4rem;
+
         a {
             font-size: 2rem;
             color: white;
@@ -233,18 +218,22 @@ export default {
             padding: 1.2rem 0;
         }
     }
+
     .ps-drawer__user {
         padding: 0 1.6rem 1.6rem;
         position: relative;
+
         .v-list-item {
             color: white;
             font-size: 2rem;
             padding: 0;
         }
+
         .ri-user-line {
             font-size: 2rem;
             margin-right: 0.8rem;
         }
+
         .user-header {
             display: flex;
             align-items: center;
@@ -253,12 +242,14 @@ export default {
             display: block;
             padding: 0 0.8rem;
         }
+
         :deep(.v-list-group__header) {
             position: absolute;
             top: -0.6rem;
             left: 0;
             right: 0;
         }
+
         :deep(.v-list-group) {
             .mdi.mdi-chevron-down {
                 color: white;
@@ -266,11 +257,13 @@ export default {
                 top: 1rem;
                 right: 1rem;
             }
+
             a {
                 padding: 1.2rem;
             }
         }
     }
+
     .ps-drawer__bottom {
         padding-top: 3.2rem;
     }
