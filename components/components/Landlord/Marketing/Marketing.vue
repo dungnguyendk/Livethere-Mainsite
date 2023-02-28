9da380dde93bb0bd143dd5047fffe914e6c19530
HAI VU <thienhai.vu@asiaesolutions.com>
2023-02-28 14:51:34 +0700

    remove snackbar on component

--- components/components/Landlord/Marketing/Marketing.vue
+++ components/components/Landlord/Marketing/Marketing.vue
@@ -1,51 +1,14 @@
 <template lang="html">
     <div class="container">
         <MarketingInventoryTable />
-        <!-- <v-snackbar v-model="snackbarActive" :timeout="2000" top right text color="green darken-4">
-            <span class="message--snackBar">
-                <i class="ri-information-line" /> {{ snackbarMessageActive }}
-            </span>
-        </v-snackbar> -->
-        <SuccessSnackBar :open="snackbarActive" :message="snackbarMessageActive" />
     </div>
 </template>
 
 <script>
 import MarketingInventoryTable from "~/components/components/Landlord/Marketing/Table/MarketingInventoryTable.vue"
-import SuccessSnackBar from "~/components/shared/Snackbar/SuccessSnackBar.vue"
-import { mapState } from "vuex";
 export default {
     name: "Marketing",
-    components: { MarketingInventoryTable, SuccessSnackBar },
-    data() {
-        return {
-            snackbarActive: false,
-            snackbarMessageActive: "Your message has been sent.",
-        }
-    },
-    computed: {
-        ...mapState({
-            snackbar: (state) => state.marketing.snackbar,
-            snackbarMessage: (state) => state.marketing.snackbarMessage
-        }),
-    },
-    methods: {
-        setSnackBar() {
-            this.snackbarActive = false
-            if (this.snackbar) {
-                this.snackbarActive = this.snackbar
-                this.snackbarMessageActive = this.snackbarMessage
-                setTimeout(() => {
-                    this.$store.commit("marketing/setSnackbar", false)
-                }, 2000)
-            }
-        }
-    },
-    watch: {
-        snackbar() {
-            this.setSnackBar()
-        },
-    }
+    components: { MarketingInventoryTable }
 }
 </script>
 
