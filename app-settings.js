681c4cffbfd0b402ad509a9771c4df35aab144d3
minhnghiadev <minhnghiadev@gmail.com>
2023-03-03 09:59:33 +0700

    Add zoho widget for landlord

--- app-settings.js
+++ app-settings.js
@@ -1,7 +1,6 @@
 const settings = [
     {
         env: "develop",
-        //base_url: "http://vnapi.asiaesolutions.com/cmspublicapi",
         media_url: "http://vnapi.asiaesolutions.com/publiccontentapi",
         postal_url: "https://apivo.aestechgroup.com/aespostal",
         mediaURLPath: "http://vnapi.asiaesolutions.com/content",
@@ -9,7 +8,6 @@ const settings = [
     },
     {
         env: "beta",
-        //base_url: "https://livethereapi.asiaesolutions.com/erppublic",
         media_url: "https://livethereapi.asiaesolutions.com/assetfileapi",
         postal_url: "https://apivo.aestechgroup.com/aespostal",
         mediaURLPath: "https://livethereapi.asiaesolutions.com/content/asset/",
@@ -17,7 +15,6 @@ const settings = [
     },
     {
         env: "production",
-        //base_url: "https://livethereapi.asiaesolutions.com/erppublic",
         media_url: "https://api.livethere.com/landlordfileapi",
         postal_url: "https://apivo.aestechgroup.com/aespostal",
         mediaURLPath: "https://api.livethere.com/content/asset/", // need update
@@ -26,7 +23,6 @@ const settings = [
 ]
 export const CURRENT_ENV = "develop"
 export const currentSettings = settings.find((item) => item.env === CURRENT_ENV)
-
 export const appSettings = {
     baseURL: currentSettings.landlord,
     siteName: "Livethere"
